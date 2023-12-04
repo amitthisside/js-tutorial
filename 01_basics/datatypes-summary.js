da4bd16d392e2ae214075d55b02b40a04f7f7f07
Amit Mishra <90273277+amitthisside@users.noreply.github.com>
2023-12-04 14:32:10 +0000

    datatypes summarized

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const rappers = ["J. Cole", "Kendrick Lamar", "Eminem", "Biggie Smalls"]
+
+const myObj = {
+    name : "Amit",
+    age : 20,
+    salary : 100000
+}
+
+const myFun = function() {
+    console.log("Hey fellas!");
+}
+
+console.log(typeof rappers);
+console.log(typeof myObj);
+console.log(typeof myFun);
