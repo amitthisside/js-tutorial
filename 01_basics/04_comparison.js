0ba2ecea4a8f3c48df6908e7ceb03b52edb1b58a
Amit Mishra <90273277+amitthisside@users.noreply.github.com>
2023-12-04 14:16:01 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log("2" == 2);
+console.log("2" === 2); // strict check
+
+//avoid confusing comparisons, prioritize clean codes
